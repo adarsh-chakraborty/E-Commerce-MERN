df08952b6dbaa8b01bacb338d0484a2322cf4c40
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-12-30 14:58:19 +0530

    Added Routing

--- front-end/README.md
+++ front-end/README.md
@@ -10,3 +10,49 @@
 ```
 npm i react-bootstrap
 ```
+
+## Install React Router
+
+```
+npm i react-router-dom
+```
+
+## Install React Router Bootstrap
+
+It's helpful if we want to wrap React Bootstrap componets with `Link` tag.
+
+```
+npm i react-router-bootstrap
+```
+
+**Usage:**
+
+Import `LinkContainer` from the package.
+
+```javascript
+import { LinkContainer } from 'react-router-bootstrap';
+```
+
+Then, we can simply use this container to wrap bootstrap components and provide a `to` prop.
+Also, make sure to remove all the `href` props.
+
+```javascript
+ <LinkContainer to="/">
+    <Navbar.Brand>Fmazon</Navbar.Brand>
+</LinkContainer>
+
+<Navbar.Collapse id="basic-navbar-nav">
+<Nav className="ms-auto">
+    <LinkContainer to="/cart">
+    <Nav.Link>
+        <i className="fas fa-shopping-cart me-1"></i>Cart
+    </Nav.Link>
+    </LinkContainer>
+    <LinkContainer to="/login">
+    <Nav.Link>
+        <i className="fas fa-user me-1"></i>Sign In
+    </Nav.Link>
+    </LinkContainer>
+</Nav>
+</Navbar.Collapse>
+```
--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,5 +1,6 @@
 // rafce
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -7,16 +8,23 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
         <Container>
-          <Navbar.Brand href="/">Fmazon</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Fmazon</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart me-1"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-user me-1"></i>Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart me-1"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user me-1"></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
