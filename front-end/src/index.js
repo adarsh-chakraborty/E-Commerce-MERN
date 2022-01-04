700fb307bed0b4efcf22d571fcc5860082054b5f
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-01-04 14:39:20 +0530

    Added Redux into Homescreen

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
